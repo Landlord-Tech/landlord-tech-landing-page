5612a770cfd8bb75bb881b91270476552d12b03c
fditata <fditata@gmail.com>
2023-12-02 09:58:43 -0300

    Fixed map function when FAQ is null

--- src/components/faq.js
+++ src/components/faq.js
@@ -4,35 +4,39 @@ import ReactMarkdown from "react-markdown"
 import Icon from "./Icon"
 
 const Faq = ({ data }) => {
+  // Check if data is an array before mapping
+  if (!Array.isArray(data)) {
+    // You can either return null, an empty fragment, or some placeholder content
+    return null; // or <>No FAQs available</> or any other placeholder you prefer
+  }
+
   return (
     <>
-      {data.map((q, i) => {
-        return (
-          <Collapsible
-            key={i}
-            transitionTime={300}
-            trigger={
-              <div className="trigger-inner">
-                <h4 className="h4">{q.faqQuestion}</h4>
-                <Icon
-                  icon="plus"
-                  className="plus"
-                  color="#007FFF"
-                  size={24}
-                />{" "}
-                <Icon
-                  icon="minus"
-                  className="minus"
-                  color="#007FFF"
-                  size={24}
-                />
-              </div>
-            }
-          >
-            <ReactMarkdown>{q.faqAnswer}</ReactMarkdown>
-          </Collapsible>
-        )
-      })}
+      {data.map((q, i) => (
+        <Collapsible
+          key={i}
+          transitionTime={300}
+          trigger={
+            <div className="trigger-inner">
+              <h4 className="h4">{q.faqQuestion}</h4>
+              <Icon
+                icon="plus"
+                className="plus"
+                color="#007FFF"
+                size={24}
+              />
+              <Icon
+                icon="minus"
+                className="minus"
+                color="#007FFF"
+                size={24}
+              />
+            </div>
+          }
+        >
+          <ReactMarkdown>{q.faqAnswer}</ReactMarkdown>
+        </Collapsible>
+      ))}
     </>
   )
 }
