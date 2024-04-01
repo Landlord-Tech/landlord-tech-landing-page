b03c3c6039bc9e2ca01caf07b3d0b0994cba74b0
fditata <fditata@gmail.com>
2024-04-01 11:18:36 -0300

    Fixed Broken Links

--- src/components/Badges.js
+++ src/components/Badges.js
@@ -5,14 +5,14 @@ export default () => {
   return (
     <section className="badges-section">
       <div className="content-wrapper">
-        <a href="https://www.capterra.com/reviews/264255/OurPetPolicy?utm_source=vendor&utm_medium=badge&utm_campaign...">
+        <a target="_blank" href="https://www.capterra.com/p/77050/OurPetPolicy/">
           <img className="capterra-logo" border="0" alt="Capterra" src="https://assets.capterra.com/badge/03405d7cb382d4172c96486f84c03e46.svg?v=2194398&p=264255" />
         </a>
-        <a href="https://www.softwareadvice.com/contract-management/our-pet-policy-profile">
+        <a target="_blank" href="https://www.softwareadvice.com/contract-management/our-pet-policy-profile">
           <img className="software-logo" border="0" alt="Software Advice" src="https://badges.softwareadvice.com/reviews/c7582ddc-fe84-43bb-938d-2d5f9a9a6dfa" />
         </a>
-        <a href="https://www.getapp.com/real-estate-property-software/a/ourpetpolicy/reviews/">
-          <img className="getapp-logo" border="0" alt="GetApp" src="https://www.getapp.com/ext/reviews_widget/v1/dark/ourpetpolicy-application" />
+        <a target="_blank" href="https://www.getapp.com/real-estate-property-software/a/ourpetpolicy/#reviews">
+          <img className="getapp-logo" border="0" alt="GetApp" src="https://www.getapp.com/ext/reviews_widget/v1/dark/5.0.png" />
         </a>
       </div>
     </section>
